39dd99164a328a7b0a3716a5e564ff858ddc6aa4
Sayali Wagal <sayaliwagal90@gmail.com>
2025-12-15 01:25:52 +0530

    Fix API baseURL and Netlify routing

--- gym-frontend/src/api/api.js
+++ gym-frontend/src/api/api.js
@@ -1,12 +1,14 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://um-gym-management-system-0jfg.onrender.com"
+  baseURL: "https://um-gym-management-system-0jfg.onrender.com/api",
 });
 
 API.interceptors.request.use((req) => {
   const token = localStorage.getItem("token");
-  if (token) req.headers.Authorization = `Bearer ${token}`;
+  if (token) {
+    req.headers.Authorization = `Bearer ${token}`;
+  }
   return req;
 });
 
